3d3ae2deaf3a2a5494db31dfc92efd00f72b3754
junior <junior@edenfarm.id>
2023-02-16 12:26:13 +0700

    initial D

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+    <v-app>
+        <div id="microApp">
+            <v-main>
+                <router-view></router-view>
+            </v-main>
+        </div>
+    </v-app>
+</template>
+<script>
+export default {
+    watch: { '$route' (val) { this.setRouter(val) } },
+    created() {
+        this.setRouter(this.$router.history.current)
+    },
+    methods : {
+        setRouter(val) {
+            localStorage.setItem("route", JSON.stringify(val));
+        }
+    }
+}
+</script>
---
+++ src/set-public-path.js
@@ -0,0 +1,3 @@
+import { setPublicPath } from "systemjs-webpack-interop";
+
+setPublicPath("@vue-mf/promotion", 2); //rename boilerplate to your module name
